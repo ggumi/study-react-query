383c2feb9e7781ce5221e147d758893e2d3d8a0c
geumhee <rmagml9769@gmail.com>
2024-08-18 16:39:50 +0900

    react qeury

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import HomePage from './HomePage';
+import ReactQueryPage from './ReactQueryPage';
+import NormalPage from './NormalPage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav style={{ backgroundColor: "beige", padding: "20px" }}>
+        <Link to="/" style={{ marginRight: "10px" }}>
+          Homepage
+        </Link>
+        <Link to="/normal-fetch" style={{ marginRight: "10px" }}>
+          normal fetch
+        </Link>
+        <Link to="/react-query">React Query</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/normal-fetch" element={<NormalPage />} />
+        <Route path="/react-query" element={<ReactQueryPage />} />
+      </Routes>
     </div>
   );
 }
